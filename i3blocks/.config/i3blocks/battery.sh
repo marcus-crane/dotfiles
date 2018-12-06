23de7367f4346835c237bac86100dc5782d422ca
Marcus Crane <marcus@utf9k.net>
2018-12-07 12:01:49 +1300

    Updated i3 stuff

--- i3blocks/.config/i3blocks/battery.sh
+++ i3blocks/.config/i3blocks/battery.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 BAT=$(acpi -b | grep -E -o '[0-9][0-9]?%')
+REMAINING=$(acpi -b | grep -E -o '[0-9][0-9]:[0-9][0-9]')
 
-echo "Battery: $BAT"
-echo "BAT: $BAT"
+echo "Battery: $BAT ($REMAINING)"
+echo "BAT: $BAT ($REMAINING)"
 
 [ ${BAT%?} -le 5 ] && exit 33
 [ ${BAT%?} -le 20 ] && echo "#FF8000"
