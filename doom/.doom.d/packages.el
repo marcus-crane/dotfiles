638b965ceec41927bc3f44c26faf4e5aec004795
Marcus Crane <marcus@utf9k.net>
2020-05-18 20:13:32 +1200

    Remove some unused packages and change back source style for notes

--- doom/.doom.d/packages.el
+++ doom/.doom.d/packages.el
@@ -45,7 +45,5 @@
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
-(package! org-download)
 (package! org-cliplink)
-(package! org-ref)
 (package! evil-escape :disable t)
