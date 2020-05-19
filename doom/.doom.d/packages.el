069f9ec4463db87c08de25e239570fd7cfa6853e
Marcus Crane <marcus@utf9k.net>
2020-05-19 22:46:40 +1200

    Reset templates back to public/private

--- doom/.doom.d/packages.el
+++ doom/.doom.d/packages.el
@@ -45,6 +45,8 @@
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
+(package! org-roam
+  :recipe (:host github :repo "org-roam/org-roam"))
 (package! org-cliplink)
 (package! org-chef)
 (package! evil-escape :disable t)
