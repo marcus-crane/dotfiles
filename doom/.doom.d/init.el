bc76a883989eaa1ff338f8e9f6679cc861155ad2
Marcus Crane <marcus@utf9k.net>
2020-06-07 23:40:09 +1200

    Disabled company due to bugginess

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -19,7 +19,7 @@
        ;;japanese
 
        :completion
-       (company +childframe) ; the ultimate code completion backend
+       ;(company +childframe) ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ivy               ; a search engine for love and life
