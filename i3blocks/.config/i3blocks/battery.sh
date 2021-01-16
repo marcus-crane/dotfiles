149772d76af63f0bfed7780dc8404b4517ddcd02
Marcus Crane <marcus@utf9k.net>
2021-01-16 14:39:51 +1300

    Fix shellcheck linting

--- i3blocks/.config/i3blocks/battery.sh
+++ i3blocks/.config/i3blocks/battery.sh
@@ -4,13 +4,13 @@ BAT=$(acpi -b | grep -E -o '[0-9][0-9]?%')
 REMAINING=$(acpi -b | grep -E -o '[0-9][0-9]:[0-9][0-9]')
 ADAPTOR=$(acpi -a | grep -E '[A-Za-z]+-[A-Za-z]+')
 
-if [[ !$REMAINING && ADAPTER == "on-line" ]]; then
+if [[ ! $REMAINING && ADAPTOR == "on-line" ]]; then
   echo "Battery Charged"
 else
   echo "Battery: $BAT ($REMAINING)"
   echo "BAT: $BAT ($REMAINING)"
-  [ ${BAT%?} -le 5 ] && exit 33
-  [ ${BAT%?} -le 20 ] && echo "#FF8000"
+  [ "${BAT%?}" -le 5 ] && exit 33
+  [ "${BAT%?}" -le 20 ] && echo "#FF8000"
 fi
 
 exit 0
