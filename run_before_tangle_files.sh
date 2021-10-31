f8c54269d2ce6a2684c04a0147c403c3f1365f9c
Marcus Crane <marcus@utf9k.net>
2021-11-01 00:15:07 +1300

    Greatly prune zshrc

--- run_before_tangle_files.sh
+++ run_before_tangle_files.sh
@@ -3,4 +3,4 @@
 while IFS= read -r -d '' file
 do
   lugh -f "$file"
-done <   <(find "$(chezmoi source-path)" -type f -name '*.md' ! -name 'README.md' -print0)
+done <   <(find "$(chezmoi source-path)" -type f -name '*.md' ! -name 'README.md' ! -name 'zshrc_graveyard.md' -print0)
