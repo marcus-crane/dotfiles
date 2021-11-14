a3676eb04fc0f98e3e2042af68a0f8e2d2e64fdf
Marcus Crane <marcus@utf9k.net>
2021-11-15 00:11:43 +1300

    Bump up font size

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -25,8 +25,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 10))
-(setq doom-theme 'doom-dracula)
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 12))
+(setq doom-theme 'doom-dracula))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
