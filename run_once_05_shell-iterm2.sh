717424a5f06703a0f62754f040d1dc4314e83926
Marcus Crane <marcus@utf9k.net>
2022-11-30 16:04:21 +1300

    Increase verbosity of install commands

--- run_once_05_shell-iterm2.sh
+++ run_once_05_shell-iterm2.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl --silent -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash  >/dev/null 2>&1 || true && echo "~ iterm2 utilities have been installed"
+curl --silent -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash && echo "~ iterm2 utilities have been installed"
--- run_once_99_shell_completions.sh
+++ run_once_99_shell_completions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # fzf shell completion
-"$(brew --prefix)"/opt/fzf/install --all  >/dev/null 2>&1 || true && echo "~ fzf shell completion has been installed"
+"$(brew --prefix)"/opt/fzf/install --all && echo "~ fzf shell completion has been installed"
