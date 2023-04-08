e1edc8b59587e4b0519bfeeedb80dd437fbc6ae3
Marcus Crane <marcus@utf9k.net>
2023-04-08 20:03:41 +1200

    Update home metadata
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -30,8 +30,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title="Home"
+      description="Browse through my annotated dotfiles">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
