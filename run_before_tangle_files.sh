93052b3118b202a9177042e0d80f86fe64a18571
Marcus Crane <marcus@utf9k.net>
2024-10-28 15:50:12 +1300

    Fix arch package install + echo output from lugh
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- run_before_tangle_files.sh
+++ run_before_tangle_files.sh
@@ -2,5 +2,5 @@
 
 while IFS= read -r -d '' file
 do
-  ~/.bin/lugh -f "$file" >/dev/null 2>&1 || true && echo "~ $file has been tangled"
+  ~/.bin/lugh -f "$file" && echo "~ $file has been tangled"
 done <   <(find "$(chezmoi source-path)" -type f -name '*.md' ! -path './docs/index.md' ! -name 'playground.md' ! -name 'README.md' ! -name 'zshrc-graveyard.md' -print0)
