71b70de52bc38df35f3f63ff0c4e04668f0458d0
Marcus Crane <marcus@utf9k.net>
2024-12-27 18:13:59 +1300

    Don't try to tangle files used for site
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- run_before_tangle_files.sh
+++ run_before_tangle_files.sh
@@ -3,4 +3,4 @@
 while IFS= read -r -d '' file
 do
   ~/.bin/lugh -f "$file" && echo "~ $file has been tangled"
-done <   <(find "$(chezmoi source-path)" -type f -name '*.md' ! -path './docs/index.md' ! -name 'playground.md' ! -name 'README.md' ! -name 'zshrc-graveyard.md' -print0)
+done <   <(find "$(chezmoi source-path)" -type f -name '*.md' ! -path './docs/index.md' ! -name 'playground.md' ! -name 'README.md' ! -name 'zshrc-graveyard.md' ! -name 'todo.md' ! -name 'index.md' -print0)
