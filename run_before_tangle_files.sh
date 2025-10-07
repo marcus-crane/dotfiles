7e2a1cbdac4d157c6d56a7db6ce7de1db55e1580
Marcus Crane <marcus@utf9k.net>
2025-10-07 15:31:17 +1300

    Tidy up tangling by only operating on Markdown file with an output key
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- run_before_tangle_files.sh
+++ run_before_tangle_files.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-while IFS= read -r -d '' file
-do
-  ~/.bin/lugh -f "$file" && echo "~ $file has been tangled"
-done <   <(find "$(chezmoi source-path)" -type f -name '*.md' ! -path './docs/index.md' ! -name 'playground.md' ! -name 'README.md' ! -name 'zshrc-graveyard.md' ! -name 'todo.md' ! -name 'index.md' -print0)
+LUGH_BIN="${HOME}/.bin/lugh"
+
+if [ ! -x "$LUGH_BIN" ]; then
+  echo "Skipping tangling as lugh not found at expected location of $LUGH_BIN"
+  exit 0
+fi
+
+SOURCE_PATH="$(chezmoi source-path)"
+
+while IFS= read -r -d '' file; do
+  if head -n 10 "$file" | grep -q "^output:"; then
+    "$LUGH_BIN" -f "$file" && echo "~ $file has been tangled"
+  fi
+done < <(find "$SOURCE_PATH" -type f -name '*.md' -print0)
