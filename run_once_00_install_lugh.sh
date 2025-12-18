b9ebcb974229b7ee1b6ea9ca148c7c2a29db5ba8
Marcus Crane <marcus@utf9k.net>
2025-12-19 10:49:35 +1300

    Add libpcre symlink for Arch
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- run_once_00_install_lugh.sh
+++ run_once_00_install_lugh.sh
@@ -3,6 +3,11 @@
 VERSION=v0.0.3
 SYSTEM=$(uname | tr '[:upper:]' '[:lower:]')
 
+# https://github.com/comby-tools/comby/issues/94#issuecomment-532412590
+if [ -f /etc/arch-release ]; then
+  sudo ln -sf /usr/lib/libpcre.so /usr/lib/libpcre.so.3
+fi
+
 cd /tmp && \
 wget -q https://github.com/marcus-crane/lugh/releases/download/$VERSION/lugh-"$SYSTEM"-$VERSION.tar.gz -O lugh.tar.gz && \
 tar -xf lugh.tar.gz && \
