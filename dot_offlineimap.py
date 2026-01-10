3e5772c9a380a6779d8d5db33cc08bb858e9a4ac
Marcus Crane <marcus@utf9k.net>
2026-01-10 22:15:39 +1300

    Add support for fetching work emails to offlineimap3
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- dot_offlineimap.py
+++ dot_offlineimap.py
@@ -5,3 +5,8 @@ def get_pass():
     return check_output(
         ["op", "read", "op://dotfiles/offlineimap3 Homeostasis Fastmail/password"]
     ).decode().strip()
+
+def get_work_pass():
+    return check_output(
+        ["op", "read", "op://dotfiles/offlineimap3 Work Gmail/password"]
+    ).decode().strip()
