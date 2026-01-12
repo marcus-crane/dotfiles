c0aeebab2fbf6d18549d3ad291c761171d89b0f6
Marcus Crane <marcus@utf9k.net>
2026-01-12 15:53:36 +1300

    Switch back to op authenticating with 1Password desktop app
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- dot_offlineimap.py
+++ dot_offlineimap.py
@@ -3,10 +3,10 @@ from subprocess import check_output
 
 def get_pass():
     return check_output(
-        ["op", "read", "op://dotfiles/offlineimap3 Homeostasis Fastmail/password"]
+        ["op", "read", "--account", "my", "op://dotfiles/offlineimap3 Homeostasis Fastmail/password"]
     ).decode().strip()
 
 def get_work_pass():
     return check_output(
-        ["op", "read", "op://dotfiles/offlineimap3 Work Gmail/password"]
+        ["op", "read", "--account", "my", "op://dotfiles/offlineimap3 Work Gmail/password"]
     ).decode().strip()
